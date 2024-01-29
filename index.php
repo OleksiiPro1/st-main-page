605e0525f3c1bc2a4696e8b8938ba1f6144e7f9e
OleksiiPro1 <ukr.top@ukr.net>
2024-01-29 11:45:27 +0100

    start main page

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+get_header();
+?>
+    <div class="brokers-mp">
+    <div class="brokers-list-mp">
+        <div class="centered-element">
+        <a href="https://secure.tickmill.com/en/sign-up?afftoken=35103_356630" data-wpel-link="external" target="_blank" rel="nofollow external noopener noreferrer">
+							<img loading="lazy" decoding="async" src="https://smarttrader.community/wp-content/uploads/2023/06/tickmill.png" class="attachment-large size-large wp-image-15652" alt="" srcset="https://smarttrader.community/wp-content/uploads/2023/06/tickmill.png 833w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/tickmill-q8aesv4ghj5gx1j7o8qcpchzsfwfy6venkcq3d2py4.png 300w, https://smarttrader.community/wp-content/uploads/2023/06/tickmill-768x384.png 768w, https://smarttrader.community/wp-content/uploads/2023/06/tickmill-760x380.png 760w" sizes="(max-width: 833px) 100vw, 833px"></a>
+        </div class="centered-element">
+        <div>
+        <a href="https://www.markets.com/account-creation/?companyId=62&amp;cid=cx36439_450746&amp;zid=CX_Markets&amp;pid=36439&amp;mid=450746&amp;custom=1&amp;c=c-36439_450746&amp;af_sub1=450746&amp;af_sub2=CX&amp;af_sub3=1&amp;traffickey=tr-36439_450746&amp;profile_name=&amp;afp1=%5Bafp1%5D&amp;afp2=%5Bafp2%5D&amp;afp3=%5Bafp3%5D&amp;afp4=%5Bafp4%5D&amp;afp5=%5Bafp5%5D&amp;afp6=%5Bafp6%5D&amp;afp7=%5Bafp7%5D&amp;afp8=%5Bafp8%5D&amp;intent_group=CFD&amp;u2=https://global.markets.com/&amp;u3=Online%20trading%20platform%20|%20Trade%20CFDs%20on%20FX,%20Stocks%20&amp;%20more%20|%20Markets.com#%22event%22:%22setLanguage%22,%22parameters%22:%22language%22:%22en%22,%22type%22:%22GENERAL%22,%22action%22:%22setLanguage%22" data-wpel-link="external" target="_blank" rel="nofollow external noopener noreferrer">
+		    <img loading="lazy" decoding="async" src="https://smarttrader.community/wp-content/uploads/2023/06/markets-logo.png" class="attachment-large size-large wp-image-15654" alt="" srcset="https://smarttrader.community/wp-content/uploads/2023/06/markets-logo.png 833w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/markets-logo-q8aet7ccydm73z1gow0i3rezig87q97x18u1byklp8.png 300w, https://smarttrader.community/wp-content/uploads/2023/06/markets-logo-768x384.png 768w, https://smarttrader.community/wp-content/uploads/2023/06/markets-logo-760x380.png 760w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/markets-logo-q8aet7cglxyn93i8filbnc721p6ssowasecsqq7t88.png 600w" sizes="(max-width: 833px) 100vw, 833px"></a>
+        </div>
+        <div class="centered-element">
+        <a href="https://smarttrader.community/vtmarketse1" data-wpel-link="internal" rel="follow">
+	        <img loading="lazy" decoding="async" src="https://smarttrader.community/wp-content/uploads/2023/06/vt-markets.png" class="attachment-large size-large wp-image-15656" alt="" srcset="https://smarttrader.community/wp-content/uploads/2023/06/vt-markets.png 833w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/vt-markets-q8aetlfxsw5hy4gzek3wn5uwf8apxprw36mbj3zp3w.png 300w, https://smarttrader.community/wp-content/uploads/2023/06/vt-markets-768x384.png 768w, https://smarttrader.community/wp-content/uploads/2023/06/vt-markets-760x380.png 760w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/vt-markets-q8aetlg1gghy38xr56oq6qmyyh9b05g9uc52xvmwmw.png 600w" sizes="(max-width: 833px) 100vw, 833px"></a>
+        </div class="centered-element">
+        <div>
+        <a href="https://www.puprime.net/forex-trading-account/?cxd=36447_372094&amp;affid=" data-wpel-link="external" target="_blank" rel="nofollow external noopener noreferrer">
+	        <img loading="lazy" decoding="async" src="https://smarttrader.community/wp-content/uploads/2023/06/puprime.png" class="attachment-large size-large wp-image-15660" alt="" srcset="https://smarttrader.community/wp-content/uploads/2023/06/puprime.png 833w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/puprime-q8aeu72862z3d5lkwbgbqiei33c5ur5pu5mhkh3n4s.png 300w, https://smarttrader.community/wp-content/uploads/2023/06/puprime-768x384.png 768w, https://smarttrader.community/wp-content/uploads/2023/06/puprime-760x380.png 760w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/puprime-q8aeu72btnbjia2cmy15a36kmcaqx6u3lb58z8quns.png 600w" sizes="(max-width: 833px) 100vw, 833px"></a> 
+        </div>
+        <div class="centered-element">
+        <a href="https://admiralmarkets.com/signup?ref_id=23651&amp;regulator=jsc&amp;utm_campaign=23651&amp;utm_medium=affiliate&amp;utm_source=partner_referral" data-wpel-link="external" target="_blank" rel="nofollow external noopener noreferrer">
+	        <img loading="lazy" decoding="async" src="https://smarttrader.community/wp-content/uploads/2023/06/admiral-markets.png" class="attachment-large size-large wp-image-15658" alt="" srcset="https://smarttrader.community/wp-content/uploads/2023/06/admiral-markets.png 595w, https://smarttrader.community/wp-content/uploads/2023/06/admiral-markets-300x111.png 300w" sizes="(max-width: 595px) 100vw, 595px"></a>
+        </div>
+        <div class="centered-element">
+        <a href="https://promo.libertex.com/lp/en-en/libertex-registration/?aff_id=49059&amp;cxd=49059_539129" data-wpel-link="external" target="_blank" rel="nofollow external noopener noreferrer">
+            <img loading="lazy" decoding="async" src="https://smarttrader.community/wp-content/uploads/2023/06/libertex-1.png" class="attachment-large size-large wp-image-15662" alt="" srcset="https://smarttrader.community/wp-content/uploads/2023/06/libertex-1.png 833w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/libertex-1-q8aeum3n7fjoiwzqghycuelvl9a19wtf88298whcd8.png 300w, https://smarttrader.community/wp-content/uploads/2023/06/libertex-1-768x384.png 768w, https://smarttrader.community/wp-content/uploads/2023/06/libertex-1-760x380.png 760w, https://smarttrader.community/wp-content/uploads/2023/06/elementor/thumbs/libertex-1-q8aeum3quzw4o1gi74j6dzdy4i8mcchszdl0no4jw8.png 600w" sizes="(max-width: 833px) 100vw, 833px"></a>
+        </div>
+    </div>
+    </div> 
+    
+    <div class="instagram-mp">
+
+        <div class='embedsocial-stories' data-ref="8aca5b5abc3657bd93e152e9ab01715d5ba5a79f"></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/st.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialStoriesScript"));</script>
+
+    </div>
+
+    <div class="posts-mp">
+    <div class="posts-list-mp">
+        <h2>Recent News & Analysis</h2>
+    </div>
+</div>
+
+<div class="posts-mp">
+    <div class="posts-list-mp">
+                     
+        <?php 
+            if (have_posts()) : 
+                while (have_posts()) : 
+                    the_post();        
+                    echo '<div class="post-mp">';
+                    // Display the post thumbnail
+                    if (has_post_thumbnail()) {
+                        echo '<div class="post-thumbnail"><a href="' . get_permalink() . '">';
+                        the_post_thumbnail('full'); // You can change 'full' to other sizes like 'medium', 'thumbnail', etc.
+                        echo '</a></div>'; 
+                    }
+
+                    // Post content
+                    echo '<article class="' . implode(' ', get_post_class()) . '" id="post-' . get_the_ID() . '" data-post-id="' . get_the_ID() . '">';
+                        echo '<a href="' . get_permalink() . '"><h1>' . get_the_title() . '</h1></a>';
+                        the_content();
+                        
+                    echo '</article>';
+                    echo '</div>';
+                endwhile; 
+            else :  
+                echo 'No posts';
+            endif; 
+        ?>       
+    
+    </div>
+</div>
+
+<div class="parent-main-page-first-text">
+    <div class="main-page-first-text">
+        <h2>
+            What are SmartTrader Trading Signals?
+        </h2>
+        <br>
+        <p>
+           SmartTrader Trading Signals are based on the comprehensive fundamental analysis of Robert Lindner and the SmartTrader Analyst Team. By translating complex market data into actionable insights = trading signals, we give our members the opportunity to mirror these suggestions (sometimes called "copy trading"). Although this mirroring mechanism can already enable consistent profitability, we primarily encourage our members to understand market dynamics and educate themselves with our guidance and the support of our team. We want every member to know what is moving the markets at any given moment and how they can profit from those movements. This way, our members can also refine their strategy on their own, use the trading software efficiently and optimize their risk management.
+        </p>
+    </div>
+</div>
+
+<div class="parent-main-page-first-text">
+    <div class="main-page-first-text">
+        <h2>
+          What do you receive at SmartTrader?
+        </h2>
+        <br>
+        <p>
+        When you join SmartTrader, you get more than "just" the best trading signals in the industry. You get access to a vast reservoir of fundamental market analysis written and led by Robert Lindner, his team and other market experts. We provide you with a personal account manager who will make sure you learn the art of trading, understand how to successfully use our fundamental and sentiment analysis, and everything you need to improve your trading results. We also provide you with ongoing support, educational opportunities and a platform to exchange ideas with other traders and market experts.
+        </p>
+    </div>
+</div>
+
+<?php
+get_footer();
